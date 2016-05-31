dd0e40ebfdf78b45532e69d09e94bd5d3cf6c187
Thomas Gassner <thomas.gassner@avocodo.com>
2016-05-31 09:51:30 +0200

    Über uns

--- ueberuns.php
+++ ueberuns.php
@@ -8,7 +8,7 @@
 <br>
 <p>Seit dieser Zeit spielt BLECHOPHON bei Früh-bzw. Dämmerschoppen, Polterabenden sowie Weihnachts – und Firmenfeiern frei nach unserem Motto „Musik mit Begeisterung und Leidenschaft auf hohem Niveau“.</p>
 <br>
-<p>Doch nicht nur Unterhaltungsmusik zählt zum Repertoire von BLECHOPHON sondern auch konzertante Blechbläsermusik wie mehrmalige Erfolge beim Wettbewerb Musik in kleinen Gruppen, ein Gemeinschaftskonzert mit dem Wartberger Chor „Pro Musica“ und ein eigenes Konzert mit dem Titel „Blechophon in Concert“ im Jahr 2014 zeigen.</p>
+<p>Doch nicht nur Unterhaltungsmusik zählt zum Repertoire von BLECHOPHON sondern auch konzertante Blechbläsermusik wie mehrmalige Erfolge beim Wettbewerb Musik in kleinen Gruppen, ein Gemeinschaftskonzert mit dem Wartberger Chor „Pro Musica“ und eigene Konzerte mit dem Titel „Blechophon in Concert“ im Jahr 2014 und „Blechophon XL in Concert“ im Jahr 2016 zeigen.</p>
 <br>
 <br>
 <p><span style="font-weight: bolder">BLECHOPHON</span> immer die richtige Wahl für gute Unterhaltung und Stimmung bei Ihrer Veranstaltung.</p>
